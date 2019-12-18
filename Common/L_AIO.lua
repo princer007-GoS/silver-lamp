5abe6c5484ab51a06b956324d02302a24a56b0c5
princer007-GoS <princer0072@gmail.com>
2019-12-18 18:06:06 +0200

    Initial for commons

---
+++ Common/L_AIO.lua
@@ -0,0 +1,5 @@
+﻿class 'L_AIO'
+
+	function L:CanCast(spell)
+		return myHero:GetSpellData(spell).currentCd == 0 and myHero:GetSpellData(spell).level > 0 and myHero:GetSpellData(spell).mana <= myHero.mana and Game.CanUseSpell(spell) == 0
+	end

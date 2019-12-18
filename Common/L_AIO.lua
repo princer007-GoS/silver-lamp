440e6de1b7d06448253f318cc02e629af5bd8cce
princer007-GoS <princer0072@gmail.com>
2019-12-18 18:28:31 +0200

    CC

--- Common/L_AIO.lua
+++
@@ -1,5 +0,0 @@
-﻿class 'L_AIO'
-
-	function L:CanCast(spell)
-		return myHero:GetSpellData(spell).currentCd == 0 and myHero:GetSpellData(spell).level > 0 and myHero:GetSpellData(spell).mana <= myHero.mana and Game.CanUseSpell(spell) == 0
-	end
---
+++ Common/L_Core.lua
@@ -0,0 +1,15 @@
+﻿class 'L_AIO'
+
+	function L:CanCast(spell)
+		return myHero:GetSpellData(spell).currentCd == 0 and myHero:GetSpellData(spell).level > 0 and myHero:GetSpellData(spell).mana <= myHero.mana and Game.CanUseSpell(spell) == 0
+	end
+	
+	function L:LoadDependency(name)
+		local file = COMMON_PATH .. name .. ".lua"
+		if not FileExist(file) then
+			print(name .. " installed")
+			DownloadFileAsync("https://raw.githubusercontent.com/princer007-GoS/silver-lamp/master/Common/" .. name .. ".lua", file, function() end)
+			while not FileExist(file) do end
+		end
+		return true
+	end
